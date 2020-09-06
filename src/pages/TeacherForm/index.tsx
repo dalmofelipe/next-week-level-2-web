a378a35a72ca2dea8009bd2e2f1d9426b67a7113
Dalmo Felipe <dalmo.felipe@gmail.com>
2020-09-06 00:02:56 -0300

    add campos para dados da aula e footer para formulario

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -3,6 +3,8 @@ import React from 'react'
 import Input from '../../components/Input'
 import PageHeader from '../../components/PageHeader'
 
+import warningImg from '../../assets/images/icons/warning.svg'
+
 import './styles.css'
 
 function TeacherForm() {
@@ -15,11 +17,29 @@ function TeacherForm() {
 
       <main>
         <fieldset>
-          <legend>Seus dados</legend>
+          <legend>Seus Dados</legend>
           <Input label="Nome Completo" name="name" />
           <Input label="Avatar" name="avatar" />
           <Input label="WhatsApp" name="whatsapp" />
         </fieldset>
+
+        <fieldset>
+          <legend>Sobre a Aula</legend>
+          <Input label="Matéria" name="subject" />
+          <Input label="Custo da sua hora por aula" name="cost" />
+        </fieldset>
+
+        <footer>
+          <p>
+            <img src={warningImg} alt="Alerta preencha todos os campos!"/>
+            Importante! <br />
+            Preencha todos os campos!
+          </p>
+
+          <button>
+            Salva Cadastro
+          </button>
+        </footer>
       </main>
     </div>
   )
